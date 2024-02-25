e933cea9be2b98cafa87e91d92dcbef5ec628412
Snorlax <zelenski.artem@mail.ru>
2024-02-25 18:59:59 +0300

    implementing Auth Bloc

--- lib/views/notes/notes_view.dart
+++ lib/views/notes/notes_view.dart
@@ -1,11 +1,16 @@
+// ignore_for_file: use_build_context_synchronously
+
 import 'package:firetest/constants/routes.dart';
 import 'package:firetest/enums/menu_action.dart';
 import 'package:firetest/services/auth/auth_service.dart';
+import 'package:firetest/services/auth/bloc/auth_bloc.dart';
+import 'package:firetest/services/auth/bloc/auth_event.dart';
 import 'package:firetest/services/cloud/cloud_note.dart';
 import 'package:firetest/services/cloud/firebase_cloud_storage.dart';
 import 'package:firetest/utilities/dialogs/logout_dialog.dart';
 import 'package:firetest/views/notes/notes_list_view.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
 
 class NotesView extends StatefulWidget {
   const NotesView({super.key});
@@ -43,9 +48,7 @@ class _NotesViewState extends State<NotesView> {
                 case MenuAction.logout:
                   final shouldLogout = await showLogOutDialog(context);
                   if (shouldLogout) {
-                    await AuthService.firebase().logOut();
-                    Navigator.of(context)
-                        .pushNamedAndRemoveUntil(loginRoute, (route) => false);
+                    context.read()<AuthBloc>().add(const AuthEventLogOut());
                   }
               }
             },
